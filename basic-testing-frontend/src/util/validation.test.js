2f73c062c7ad9d68bc2f102f0ae4c8dc3a751dd4
andres alcocer <devandres.tech@gmail.com>
2022-05-12 20:29:41 -0400

    implemented test suites

--- basic-testing-frontend/src/util/validation.test.js
+++ basic-testing-frontend/src/util/validation.test.js
@@ -1,33 +1,37 @@
-import { it, expect } from 'vitest'
+import { it, expect, describe } from 'vitest'
 
 import { validateNumber, validateStringNotEmpty } from './validation'
 
-it('validateStringNotEmpty - should throw an error if empty string is passed', () => {
-  const input = ''
-  const result = () => {
-    validateStringNotEmpty(input)
-  }
+describe('validateStringNotEmpty()', () => {
+  it('validateStringNotEmpty - should throw an error if empty string is passed', () => {
+    const input = ''
+    const result = () => {
+      validateStringNotEmpty(input)
+    }
 
-  expect(result).toThrow('Invalid input - must not be empty.')
+    expect(result).toThrow('Invalid input - must not be empty.')
+  })
 })
 
-it('validateNumber - should throw an error if param is not of type number', () => {
-  const input = 'hello'
-  const result = () => {
-    validateNumber(input)
-  }
+describe('validateNumber()', () => {
+  it('validateNumber - should throw an error if param is not of type number', () => {
+    const input = 'hello'
+    const result = () => {
+      validateNumber(input)
+    }
 
-  expect(result).toThrow('Invalid number input.')
-})
+    expect(result).toThrow('Invalid number input.')
+  })
 
-it('should throw an error, if a non-numeric value is provided', () => {
-  const input = '1'
-  const validateFn = () => validateNumber(input)
-  expect(validateFn).toThrow()
-})
+  it('should throw an error, if a non-numeric value is provided', () => {
+    const input = '1'
+    const validateFn = () => validateNumber(input)
+    expect(validateFn).toThrow()
+  })
 
-it('should throw an error, if a number is provided', () => {
-  const input = 1
-  const validateFn = () => validateNumber(input)
-  expect(validateFn).not.toThrow()
+  it('should throw an error, if a number is provided', () => {
+    const input = 1
+    const validateFn = () => validateNumber(input)
+    expect(validateFn).not.toThrow()
+  })
 })
