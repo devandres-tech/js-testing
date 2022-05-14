9ee6a90682137f86e860178d9e4c507cd5aefe06
andres alcocer <devandres.tech@gmail.com>
2022-05-14 18:13:23 -0400

    refactored app.js file in frontend

--- basic-testing-frontend/src/math.js
+++ basic-testing-frontend/src/math.js
@@ -1,3 +1,5 @@
+import { cleanNumbers } from './util/numbers.js'
+
 // units, most of the time are functions
 export function add(numbers) {
   let sum = 0
@@ -6,4 +8,15 @@ export function add(numbers) {
     sum += +number
   }
   return sum
+}
+
+export function calculateResult(numberValues) {
+  let result = ''
+  try {
+    const numbers = cleanNumbers(numberValues)
+    result = add(numbers).toString()
+  } catch (error) {
+    result = error.message
+  }
+  return result
 }
