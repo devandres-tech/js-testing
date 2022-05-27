4c3ddd0ee0f63872173609caffbf15561060af3c
andres alcocer <devandres.tech@gmail.com>
2022-05-27 16:21:18 -0400

    created mocks to test the fs module

--- mock_spies/src/util/io.test.js
+++ mock_spies/src/util/io.test.js
@@ -1,9 +1,17 @@
-import { it, expect } from 'vitest'
+import { it, expect, vi } from 'vitest'
+import { promises as fs } from 'fs'
+
 import writeData from './io'
+
+// mocks, they are useful when trying to test external modules/dependencies
+vi.mock('fs')
 
 // crates a side effect (writes to the file system)
 it('should execute the writeFile method', () => {
   const testData = 'Test'
   const testFileName = 'test.txt'
-  return expect(writeData(testData, testFileName)).resolves.toBeUndefined()
+
+  writeData(testData, testFileName)
+
+  expect(fs.writeFile).toBeCalled()
 })
