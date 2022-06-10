a939c3c80c8d27b1ce648149fe9c88d66246940e
andres alcocer <devandres.tech@gmail.com>
2022-06-09 21:05:25 -0400

    created mocks folder to implement custom functionality

--- mock_spies/src/util/io.test.js
+++ mock_spies/src/util/io.test.js
@@ -22,6 +22,18 @@ it('should execute the writeFile method', () => {
   const testFileName = 'test.txt'
 
   writeData(testData, testFileName)
+  // return expect(writeData(testData, testFileName)).resolves.toBeUndefined()
   // expect(fs.writeFile).toBeCalled()
   expect(fs.writeFile).toBeCalledWith(testFileName, testData)
+})
+
+// crates a side effect (writes to the file system)
+it('should return a promise that resolves to no value if called correctly', () => {
+  const testData = 'Test'
+  const testFileName = 'test.txt'
+
+  writeData(testData, testFileName)
+  return expect(writeData(testData, testFileName)).resolves.toBeUndefined()
+  // expect(fs.writeFile).toBeCalled()
+  // expect(fs.writeFile).toBeCalledWith(testFileName, testData)
 })
