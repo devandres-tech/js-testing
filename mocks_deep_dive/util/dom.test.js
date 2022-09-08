c2ad7a75fa1403836b590e13a277e51c6902a365
andres alcocer <devandres.tech@gmail.com>
2022-09-08 16:51:21 -0400

    added before each hook

--- mocks_deep_dive/util/dom.test.js
+++ mocks_deep_dive/util/dom.test.js
@@ -1,4 +1,4 @@
-import { it, vi } from 'vitest'
+import { beforeEach, expect, it, vi } from 'vitest'
 import fs from 'fs'
 import path from 'path'
 import { Window } from 'happy-dom'
@@ -12,10 +12,27 @@ const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString()
 
 const window = new Window()
 const document = window.document
-// render html page virtually
-document.write(htmlDocumentContent)
+// replace it globally
 vi.stubGlobal('document', document)
 
-it('first test', () => {
-  showError('test')
+beforeEach(() => {
+  // render html page virtually and override it
+  document.body.innerHTML = ''
+  document.write(htmlDocumentContent)
+})
+
+it('should add an error paragraph to the id="errors" element', () => {
+  showError('Test')
+
+  const errorsElement = document.getElementById('errors')
+  const errorParagraph = errorsElement.firstElementChild
+
+  expect(errorParagraph).not.toBeNull()
+})
+
+it('should not contain an error paragraph initially', () => {
+  const errorsElement = document.getElementById('errors')
+  const errorParagraph = errorsElement.firstElementChild
+
+  expect(errorParagraph).toBeNull()
 })
